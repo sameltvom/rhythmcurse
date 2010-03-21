672aac7f137f87b17fb12d7c95f8c5ed752a87e4
Samuel Skanberg <dt05ss5@student.lth.se>
2010-03-21 21:24:03 +0100

    Removed swedish letter from surname

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 #    Rhythmcurse - A command line interface to Rhythmbox
-#    Copyright (C) 2010 Samuel Skånberg 
+#    Copyright (C) 2010 Samuel Skanberg 
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
